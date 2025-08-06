fa2b19835ce7e373df50b626474e53e8e2e9e8b5
imaihayato2025 <imaihayato2024@gmail.com>
2025-08-06 10:09:35 +0900

    ダークテーマ実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,59 +1,45 @@
-"use client";
-
+import ThemeToggleProvider from "./components//ThemeToggleProvider";
+import ThemeToggleButton from "./components/ThemeToggleButton";
+import { Button } from "@mui/material";
 import Link from "next/link";
-import { Box, Button, Typography } from "@mui/material";
 
-export default function Home() {
+export default function App() {
   return (
-    <Box
-      sx={{
-        minHeight: "100vh",
-        bgcolor: "background.default",
-        color: "text.primary",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        position: "relative",
-        p: 2,
-      }}
-    >
-      <Typography variant="h2" component="h1" fontWeight="bold" gutterBottom>
-        NEWS NOW
-      </Typography>
-
-      <Link href="/">
-        <Button
-          variant="contained"
+    <ThemeToggleProvider>
+      <div className="flex h-screen flex-col items-center justify-center">
+        <ThemeToggleButton
           sx={{
-            mt: 3,
-            px: 5,
-            py: 1.5,
-            borderRadius: "999px",
-            fontWeight: "bold",
-            textTransform: "none",
-            backgroundColor: "#1976d2",
-            color: "#fff",
-            transition: "transform 0.3s, backgroud-color 0.3s",
-            width: 200,
-            "&:hover": {
-              backgroundColor: "#1565c0",
-              transform: "scale(1.1)",
-            },
+            position: "fixed",
+            top: 5,
+            right: 5,
           }}
-        >
-          記事一覧へ
-        </Button>
-      </Link>
-
-      {/* 右上に配置 */}
-      <Box
-        sx={{
-          position: "absolute",
-          top: 15,
-          right: 5,
-        }}
-      ></Box>
-    </Box>
+        />
+        <h1 className="text-4xl font-bold">NEWS NOW</h1>
+        <Link href="/">
+          <Button
+            variant="contained"
+            sx={{
+              mt: 3,
+              px: 5,
+              py: 1.5,
+              borderRadius: "999px",
+              fontSize: "16px",
+              fontWeight: "bold",
+              textTransform: "none",
+              backgroundColor: "#1976d2",
+              color: "#fff",
+              transition: "transform 0.3s, background-color 0.3s",
+              width: 200, // ここで横幅を指定（px単位）
+              "&:hover": {
+                backgroundColor: "#1565c0",
+                transform: "scale(1.1)",
+              },
+            }}
+          >
+            記事一覧へ
+          </Button>
+        </Link>
+      </div>
+    </ThemeToggleProvider>
   );
 }
